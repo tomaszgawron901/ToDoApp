b87bc52491e7334a72cdad64aca138b7647ccab9
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-03-09 23:06:17 +0100

    NoteList Added.

--- src/screens/todoList/NoteView.tsx
+++ src/screens/todoList/NoteView.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react';
-import { Text } from 'react-native';
+import React, { FC, Component } from 'react';
+import { Text, DatePickerAndroid, View } from 'react-native';
 import styled from 'styled-components/native';
 
 import Colors from '../../constans/Colors';
@@ -23,23 +23,37 @@ const TitleText = styled.Text`
 const NoteText = styled.Text`
     marginTop: 10px;
     width: 100%;
-    maxHeight: 200px;
+`;
+
+const DateText = styled.Text`
+    color: ${Colors.mediumGray};
+    alignSelf: flex-end;
 `;
 
 interface INoteViewProps {
     title: string;
     text: string;
     color: NoteColors;
-    date: string;
 }
 
-const Note: FC<INoteViewProps> = props => {
-    return (
-        <NoteContainer style={{backgroundColor: props.color}} activeOpacity={0.9} >
-            <TitleText>{props.title}</TitleText>
-            <NoteText>{props.text}</NoteText>
-        </NoteContainer>
-    );
-};
+class Note extends React.PureComponent<INoteViewProps> {
+    readonly state = {minimalized: false };
+    readonly date = new Date();
+
+    _changeSize() {
+        this.setState({minimalized: !this.state.minimalized });
+    }
+
+    render() {
+        return (
+            <NoteContainer style={{backgroundColor: this.props.color}} activeOpacity={0.9} onPress={() => {this._changeSize(); }} >
+                <TitleText>{this.props.title}</TitleText>
+                <NoteText style={this.state.minimalized ? {maxHeight: undefined} : {maxHeight: 250}}>{this.props.text}</NoteText>
+                <DateText>{this.date.toLocaleDateString()}</DateText>
+            </NoteContainer>
+        );
+    }
+
+}
 
 export default Note;
