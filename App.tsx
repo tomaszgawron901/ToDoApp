55b6e278faa6339049a7486bdc4794bb7ca2a80e
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-04-18 21:58:06 +0200

    deleted redundant function (getContacts)

--- App.tsx
+++ App.tsx
@@ -1,18 +1,12 @@
 import 'react-native-gesture-handler';
 import { Provider } from 'react-redux';
 import * as Contacts from 'expo-contacts';
-import { Gyroscope } from 'expo-sensors';
 import * as React from 'react';
 
 import store from './src/tools/store';
 import Main from './src/Main';
 
 export default function App() {
-
-    const getContacts = () => {
-        Contacts.getContactsAsync();
-    };
-
     return (
         <Provider store={store}>
             <Main />
